65cb5b877313d7c7e94b437a999679286fe12921
Leo Liu <changtong1993@gmail.com>
2013-12-10 19:28:28 +0800

    index.js added

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".btn-service").bind("click", function(e){
+    $(this).css("display", "none");
+    $(".ticket").css("display", "block");
+    $(".ticket input[name='name']").focus();
+    e.stopPropagation();
+  });
+  $(document).bind("click", function(){
+    $(".ticket").css("display", "none");
+    $(".btn-service").css("display", "block");
+  });
+  $(".ticket").bind("click", function(e){
+    e.stopPropagation();
+  });
+});
